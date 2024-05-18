ed8367c4afc4e0e0415493dd88444b0a5c0fdc4f
allisonmart <agmart@uw.edu>
2024-05-18 14:01:34 -0700

    edited Report.js

--- src/components/Report.js
+++ src/components/Report.js
@@ -17,14 +17,13 @@ export function Report(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here
     console.log('Form submitted:', formData);
   };
 
   return (
     <>
       <header>
-        <img id="menu" src="img/menu-icon.png" alt="menu" />
+        <img id='menu' src='img/menu-icon.png' alt='menu' />
         <h1>MAKE A REPORT</h1>
         <p>
           Empower your community to stay vigilant and informed by making safety reports.<br />
@@ -33,26 +32,26 @@ export function Report(props) {
         </p>
       </header>
 
-      <div id="report">
+      <div id='report'>
         <section>
           <div>
-            <label htmlFor="nameInput">Name:</label> <br />
-            <input type="text" id="nameInput" name="name" value={formData.name} onChange={handleChange} /> <br />
+            <label htmlFor='nameInput'>Name:</label> <br />
+            <input type='text' id='nameInput' name='name' value={formData.name} onChange={handleChange} /> <br />
 
-            <label htmlFor="idInput">Student ID:</label> <br />
-            <input type="text" id="idInput" name="studentID" value={formData.studentID} onChange={handleChange} /> <br />
+            <label htmlFor='idInput'>Student ID:</label> <br />
+            <input type='text' id='idInput' name='studentID' value={formData.studentID} onChange={handleChange} /> <br />
 
-            <label htmlFor="phoneInput">Phone:</label> <br />
-            <input type="text" id="phoneInput" name="phone" value={formData.phone} onChange={handleChange} /> <br />
+            <label htmlFor='phoneInput'>Phone:</label> <br />
+            <input type='text' id='phoneInput' name='phone' value={formData.phone} onChange={handleChange} /> <br />
 
-            <label htmlFor="dateTimeInput">Date and Time:</label> <br />
-            <input type="datetime-local" id="dateTimeInput" name="dateTime" value={formData.dateTime} onChange={handleChange} /> <br />
+            <label htmlFor='dateTimeInput'>Date and Time:</label> <br />
+            <input type='datetime-local' id='dateTimeInput' name='dateTime' value={formData.dateTime} onChange={handleChange} /> <br />
 
-            <label htmlFor="locationInput">Location:</label> <br />
-            <input type="text" id="locationInput" name="location" value={formData.location} onChange={handleChange} /> <br />
+            <label htmlFor='locationInput'>Location:</label> <br />
+            <input type='text' id='locationInput' name='location' value={formData.location} onChange={handleChange} /> <br />
 
-            <label htmlFor="descriptionInput">Description of what you saw:</label> <br />
-            <input type="text" id="descriptionInput" name="description" value={formData.description} onChange={handleChange} /> <br />
+            <label htmlFor='descriptionInput'>Description of what you saw:</label> <br />
+            <input type='text' id='descriptionInput' name='description' value={formData.description} onChange={handleChange} /> <br />
           </div>
         </section>
 
